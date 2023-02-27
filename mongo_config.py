c370869fb94ea017c70d4912556b4fbac3d604b8
npbcts <auditx@hotmail.com>
2023-02-27 23:27:39 +0800

    创建数据库连接配置文件

---
+++ mongo_config.py
@@ -0,0 +1,8 @@
+TEST_DB1 = 'engine_learn1'
+TEST_DB2 = 'engine_learn2'
+TEST_DB3 = 'engine_learn3'
+
+HOST = '192.168.2.172'
+PORT = 27017
+USERNAME = 'student'  # 三个学习用数据库用户名相同
+PASSWORD = 'stu123'  # 三个学习用数据库密码相同
